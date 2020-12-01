15b5ff8b1589945f635ed12dceb52f7ab227282c
Max Pernklau <max.pernklau@udo.edu>
2020-12-01 23:48:30 +0100

    format with black

--- 1/find_sum.py
+++ 1/find_sum.py
@@ -4,14 +4,14 @@ target = 2020
 numbers = np.loadtxt("1/input")
 
 # generate a matrix that consists of repeated number vectors
-number_matrix = np.tile(numbers,(len(numbers),1))
+number_matrix = np.tile(numbers, (len(numbers), 1))
 # we actually only need a triangular matrix, but we are trying to keep it simple
 # so we're doing every addition twice
 add_matrix = number_matrix + number_matrix.T
 
 # we find two entries, only use one of those
-indices_of_desired_entries = np.where(add_matrix==target)[0]
+indices_of_desired_entries = np.where(add_matrix == target)[0]
 
-a,b = indices_of_desired_entries
-print(numbers[a],"+",numbers[b],"=",numbers[a]+numbers[b])
-print(numbers[a],"*",numbers[b],"=",numbers[a]*numbers[b])
+a, b = indices_of_desired_entries
+print(numbers[a], "+", numbers[b], "=", numbers[a] + numbers[b])
+print(numbers[a], "*", numbers[b], "=", numbers[a] * numbers[b])
