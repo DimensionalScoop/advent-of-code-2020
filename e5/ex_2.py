86a4e03f7814119a6028116d52be5226f2a779c7
Max Pernklau <max.pernklau@udo.edu>
2020-12-05 14:25:40 +0100

    beautify

--- e5/ex_2.py
+++ e5/ex_2.py
@@ -5,16 +5,9 @@ from ex_1 import parse_code
 passes = open("e5/input").readlines()
 passes = np.array([parse_code(p) for p in passes])
 
-sort_by_id = np.argsort(passes[:,2])
-sorted_passes = passes[sort_by_id,:]
-shift_diff = sorted_passes[:-1,2] - sorted_passes[1:,2]
+sort_by_id = np.argsort(passes[:, 2])
+sorted_passes = passes[sort_by_id, :]
+shift_diff = sorted_passes[:-1, 2] - sorted_passes[1:, 2]
 hole = np.argmin(shift_diff)
 
-print(
-    sorted_passes[hole],
-    sorted_passes[hole+1]
-    )
-
-# for r,c,id in passes:   
-#     plt.plot(c,r,'rs')
-# plt.show()
+print(sorted_passes[hole], sorted_passes[hole + 1])
